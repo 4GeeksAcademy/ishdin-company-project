bce448b6bb42391678c6d992cbc294c397e9eb6e
Ishdin <162821083+Ishdin@users.noreply.github.com>
2026-08-15 18:22:05 +0000

    added all validations rules for product carrier and shipment

--- src/utils/validations.ts
+++ src/utils/validations.ts
@@ -9,7 +9,7 @@ function validateProduct(product: Product[]): { valid: boolean, errors: string[]
 
     //sku must not be empty
     if (sampleProducts[i].sku == "") {
-      return "sku must not be empty"
+      return { valid: false, errors: ["sku must not be empty"] }
     }
 
     //weightKg must be > 0 and <= 100
@@ -45,15 +45,47 @@ function validateProduct(product: Product[]): { valid: boolean, errors: string[]
 }
 
 
+function validateShipment(shipment: Shipment[]): { valid: boolean, errors: string[] } {
+  for (let j = 0; j < shipment.length; j++) {
+    if (shipment[j].quantity < 0) {
+      return { valid: false, errors: ["quantity must be greater than 0"] }
+    }
+    if (shipment[j].declaredValueUSD < 0) {
+      return { valid: false, errors: ["declaredValueUSD must be greater than 0"] }
+    }
+    if (shipment[j].destination.distanceKm <= 0) {
+      return { valid: false, errors: ["distanceKm must be greater than or equal to 0"] }
+    }
+  }
+  return { valid: true, errors: [] }
+}
+function validateCarrier(carrier: Carrier[]): { valid: boolean, errors: string[] } {
+  for (let k = 0; k < carrier.length; k++) {
+    if (carrier[k].baseRateUSD <= 0 && carrier[k].ratePerKgUSD <= 0 && carrier[k].ratePerKmUSD <= 0) {
+      return { valid: false, errors: ["Value of baseRateUSD, ratePerKgUSD, ratePerKmUSD must be greater than or equal to 0 "] }
+    }
+    if (carrier[k].avgDeliveryDays < 0) {
+      return { valid: false, errors: ["avgDeliveryDays must be grtearer than or equal to 0"] }
+    }
+    if (carrier[k].onTimeRate < 0 || carrier[k].onTimeRate > 100) {
+      return { valid: false, errors: ["OnTimeRate must be inbetween 0 and 100"] }
+    }
+    if (carrier[k].maxWeightKg < 0) {
+      return { valid: false, errors: ["maxWeightKg must be greater than 0"] }
+    }
+    if (carrier[k].operatesIn.length === 0){
+      return { valid: false, errors: ["operatesIn must operate in at least one country"] }
+    }
+}
+return { valid: true, errors: [] }
+}
 
-
-
-
-
-
-
+// Validation rules for Product
 console.log(validateProduct(sampleProducts))
-
+// Validation rules for Shipment
+console.log(validateShipment(sampleShipment))
+// Validation rules for Carrier
+console.log(validateCarrier(sampleCarriers))
 
 
 
